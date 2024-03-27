7926dd68b26075a5251a1318162bd916c45dfcfe
Seoyeong Min <mseo1123@naver.com>
2024-03-27 22:41:39 +0100

    Setting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule],
 })
 export class AppComponent {
   title = 'todolist';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  Items = [
+    { description: 'study Angular', done: true },
+    { description: 'eat dinner', done: false },
+  ];
+
+  get filteredItems() {
+    if (this.filter === 'all') {
+      return this.Items;
+    }
+    return this.Items.filter((item: any) => {
+      this.filter === 'done' ? item.done : !item.done;
+    });
+  }
 }
